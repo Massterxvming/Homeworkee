dc1cc655cabdeba1fbe43b9161466a05420313ef
XvMing <2260947414@qq.com>
2022-10-29 16:04:56 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace first {
+    
+    class MyStack<T>
+    {
+        
+        private int stack;
+        public T[] Items;
+
+        public MyStack()
+        {
+            stack = -1;
+        }
+
+        public void InitStack()
+        {
+           
+        }
+
+        public void ClearStack()
+        {
+            stack = -1;
+        }
+
+        public bool push(T item)
+        {
+            Items[++stack] = item;
+            return true;
+        }
+
+        public T pop()
+        {
+            return Items[stack--];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack=new MyStack<int>();
+            stack.push(2);
+            
+            Console.WriteLine(stack.pop());
+        }
+    }
+}
---
+++ ConsoleApp1028/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace second
+{
+    public delegate string print(string a, int b);
+    public class pring
+    {
+        public static string agg(string a, int b)
+        {
+            return a + b.ToString();
+        }
+        public string agg1(string a, int b)
+        {
+            return a + b.ToString();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            print p = pring.agg;
+            pring pp = new pring();
+            p+= pp.agg1;
+
+            Console.WriteLine(p("string ", 1));
+        }
+    }
+}
